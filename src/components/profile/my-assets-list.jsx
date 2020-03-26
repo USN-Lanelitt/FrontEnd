99ab1eb0fbc807e35796ebd25deef77d84a5b7ba
jbg92 <jbg92@live.no>
2020-03-27 00:27:53 +0100

    my-assets-list.jsx la til getMyAssets

--- src/components/profile/my-assets-list.jsx
+++ src/components/profile/my-assets-list.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Container} from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import MyAssetsCard from "./my-assets-card";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add"
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import axios from "axios";
 
 
 const useStyles = makeStyles(theme => ({
@@ -17,30 +18,36 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const MyAssetsList = () => {
+
     const classes = useStyles();
 
-    const menuItems = [
-        {
-            title: "Eiendel",
-            description: "Her kan du se eiendeler til utlån",
-            imageUrl: "https://source.unsplash.com/random"
-        },
+    const [userId, setId] = useState(sessionStorage.getItem('userId'));
+    const [menuItems, setMenuItem] = useState([]);
 
-        {
-            title: "Eiendel",
-            description: "Her kan du se eiendeler til utlån",
-            imageUrl: "https://source.unsplash.com/random"
-        }
-    ];
+    useEffect(() => {
+        console.log("", userId, sessionStorage.getItem('userId'));
+        axios.get("/assets/getMyAsset/"+userId)
+            .then(result => {
+                if (result.status === 200) {
+                    console.log(result.data);
+                    setMenuItem(result.data);
+                }
+            })
+            .catch(e => console.log(e));
+    }, [setMenuItem, userId]);
+
+
+
+
+
     return (
         <Container>
 
             <Grid container spacing={4} justify="center">
-
                 {
                     menuItems.map(item => (
                             <Grid item>
-                                <MyAssetsCard title={item.title} description={item.description} imageUrl={item.imageUrl}/>
+                                <MyAssetsCard title={item.assetName} description={item.description} imageUrl={"https://source.unsplash.com/random"}/>
                             </Grid>
                         )
                     )
