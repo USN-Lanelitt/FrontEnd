25793b29f4a3650dcf3ffc7789a2daa6d8ae767e
Linda Loftsgarden <linda.w.loftsgarden@gmail.com>
2020-04-07 18:26:39 +0200

    fixed search bar on mobile

--- src/components/search/search-friends.jsx
+++ src/components/search/search-friends.jsx
@@ -15,7 +15,6 @@ const useStyles = makeStyles((theme) => ({
         '&:hover': {
             backgroundColor: fade(theme.palette.common.white, 0.25),
         },
-        marginRight: theme.spacing(2),
         marginLeft: 0,
         width: '100%',
         [theme.breakpoints.up('sm')]: {
@@ -34,6 +33,16 @@ const useStyles = makeStyles((theme) => ({
     },
     inputRoot: {
         color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
     },
 
 }));
@@ -68,6 +77,10 @@ const SearchFriends = () => {
                 getOptionLabel={option => option.firstName}
                 style={{width: 210}}
                 renderInput={params => <TextField   {...params}
+                                                    classes={{
+                                                        root: classes.inputRoot,
+                                                        input: classes.inputInput,
+                                                    }}
                                                   label="Søk på bruker..."
                                                   variant="filled"/>}
             />
