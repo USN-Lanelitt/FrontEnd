b85262eda2eb441199934e73d007084a1be0cbbe
nicole bendu <nic.bendu@gmail.com>
2020-04-21 23:59:43 +0200

    Chat-link i nav

--- src/components/loan/loan-request-send.jsx
+++ src/components/loan/loan-request-send.jsx
@@ -33,8 +33,8 @@ const LoanRequestSend = () => {
     const [userId, setId] = useState(sessionStorage.getItem('userId'));
     const [assetName, setAssetName] = useState('');
     const [textValue, setTextValue] = useState('');
-    const [selectedDate, setSelectedDate] = React.useState(new Date() );
-    const [selectedDate2, setSelectedDate2] = React.useState(new Date() );
+    const [selectedDate, setSelectedDate] = React.useState(moment().format("YYYY-MM-DD") );
+    const [selectedDate2, setSelectedDate2] = React.useState(moment().format("YYYY-MM-DD") );
     const {id, assetId} = useParams();
 
     function sendMessage(message) {
