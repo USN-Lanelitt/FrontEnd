28ee895ce160d342322f7211938a2116f59c183d
jbg92 <jbg92@live.no>
2020-04-24 11:44:44 +0200

    la til oversetting

--- src/feature/Assets/asset-search.jsx
+++ src/feature/Assets/asset-search.jsx
@@ -5,12 +5,9 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import HomeMenu from "../../components/home/home-menu";
-import Copyright from "../../components/home/Copyright";
 import {useParams} from "react-router";
 import Box from "@material-ui/core/Box";
-import AssetSearchTextfield from "../../components/home/AssetSearchTextfield";
+import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles(theme => ({
     heroContent: {
@@ -27,25 +24,20 @@ const AssetSearch = () => {
     const classes = useStyles();
     const {search} = useParams();
     const [searchTherm, setSearchTherm] = useState([]);
+    const { t, i18n } = useTranslation();
 
     return (
         <div>
             <div className={classes.heroContent}>
                 <Container>
                     <Typography component="h2" variant="h2" align="center" color="textPrimary" gutterBottom>
-                        {searchTherm}
-                        Søk
+                        <h3>{t('home.4')}</h3>
                     </Typography>
                 </Container>
             </div>
             <CssBaseline />
             <main>
-
-                <Container>
-                    <AssetSearchTextfield/>
-                </Container>
                 <Container className={classes.cardGrid}>
-
                    <Box m={5}>
                     <hr/>
                    </Box>
@@ -55,8 +47,6 @@ const AssetSearch = () => {
                 </Container>
             </main>
         </div>
-
     );
 };
-
 export default AssetSearch;
